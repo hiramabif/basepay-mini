bf5c091b4efaca65d6a78af61669f1e37bf6d199
hiramabif <kelvinonile@gmail.com>
2025-12-06 09:51:17 +0100

    edited route.ts and page load

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -15,7 +15,7 @@ export async function GET() {
                 name: "Pay with Base",
                 homeUrl: URL,
                 iconUrl: "https://basepay-mini.vercel.app/assets/Base_basemark_blue.png",
-                splashImageUrl: "https://basepay-mini.vercel.app/assets/Base_square_blue.png",
+                splashImageUrl: "https://basepay-mini.vercel.app/assets/Base_basemark_blue.png",
                 splashBackgroundColor: "#ffffff",
                 webhookUrl: "https://basepay-mini.vercel.app/api/webhook",
                 subtitle: "Fast, free, secure",
