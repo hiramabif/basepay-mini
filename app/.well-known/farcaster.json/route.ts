918eb87a8c060db0c22cbaf163f6a9652adc5ef3
hiramabif <kelvinonile@gmail.com>
2025-12-06 08:51:52 +0100

    updated manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,8 +1,35 @@
-import { NextResponse } from 'next/server';
+function withValidProperties(properties: Record<string, undefined | string | string[]>) {
+    return Object.fromEntries(
+        Object.entries(properties).filter(([_, value]) => (Array.isArray(value) ? value.length > 0 : !!value))
+    );
+}
 
 export async function GET() {
-    return NextResponse.redirect(
-        "https://api.farcaster.xyz/miniapps/hosted-manifest/019af0a9-8e31-7c6e-4935-8abb525a2c53",
-        { status: 307 }
-    );
+    const URL = process.env.NEXT_PUBLIC_URL || "https://basepay-mini.vercel.app";
+    return Response.json(
+        {
+            miniapp: {
+                version: "1",
+                name: "Pay with Base",
+                homeUrl: URL,
+                iconUrl: "https://basepay-mini.vercel.app/assets/Base_square_blue.png",
+                splashImageUrl: "https://basepay-mini.vercel.app/assets/Base_square_blue.png",
+                splashBackgroundColor: "#ffffff",
+                webhookUrl: "https://basepay-mini.vercel.app/api/webhook",
+                subtitle: "Fast, fun, social",
+                description: "A fast, fun way to challenge friends in real time.",
+                screenshotUrls: [
+                    "https://basepay-mini.vercel.app/assets/Base_square_blue.png",
+                ],
+                primaryCategory: "social",
+                tags: ["example", "miniapp", "baseapp"],
+                heroImageUrl: "https://basepay-mini.vercel.app/assets/Base_square_blue.png",
+                tagline: "Play instantly",
+                ogTitle: "Pay with Base",
+                ogDescription: "Send fast bulk transfers with one click.",
+                ogImageUrl: "https://basepay-mini.vercel.app/assets/Base_square_blue.png",
+                noindex: false,
+            }
+        }
+    ); // see the next step for the manifest_json_object
 }
